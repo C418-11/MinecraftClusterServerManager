90b5b8e8f5ff7b415a37bc144e4038cdfcadf613
C418____11 <553515788@qq.com>
2024-11-16 07:56:10 +0800

    小重构

--- command_tools/command.py
+++ command_tools/command.py
@@ -4,6 +4,8 @@ __author__ = "C418____11 <553515788@qq.com>"
 from functools import update_wrapper
 from functools import wraps
 from typing import Callable
+from typing import Never
+from typing import Optional
 from typing import Union
 
 from . import errors
@@ -28,32 +30,41 @@ def default_cut_rule(string: any, *_, **__):
 
 
 class Command:
-    def __init__(self, name: Union[str, float],
-                 op_level: Union[float, types.OperateLevel] = 0,
-                 *,
-                 args_maker: Callable = default_cut_rule,
-                 cut_rule=default_cut_rule,
-                 lead_char: Union[types.LeadChar, None] = None,
-                 description: str = "Not defined!",
-                 usage: str = "Not defined!",
-                 cmd_list: Union[types.CommandList, None] = None):
+    def __init__(
+            self,
+            name: Union[str, float],
+            op_level: Union[types.OperateLevel, float] = 0,
+            *,
+            args_maker: Callable = default_cut_rule,
+            cut_rule: Optional[Callable[[str], list[str]]] = None,
+            lead_char: Optional[types.LeadChar] = None,
+            description: str = "Not defined!",
+            usage: str = "Not defined!",
+            cmd_list: Optional[types.CommandList] = None
+    ):
 
         """
 
         用于注册指令
 
-        :param name: str: 指令名
-        :param op_level: Union[float, types.OperateLevel]: 需求权限等级
-        :param args_maker: Callable: 对参数修改的函数
-        :param cut_rule: Callable: 裁剪规则
-        :param lead_char: Union[LeadChar, None]: 领导符
-        :param description: str: 指令的描述
-        :param usage: str: 指令的用法 (如果第一个字符是%, 则自动添加指令名)
-        :param cmd_list: CommandList[List]: 指令注册表
+        :param name: 指令名
+        :type name: str
+        :param op_level: 需求权限等级
+        :type op_level: Union[types.OperateLevel, float]
+        :param args_maker: 对参数修改的函数
+        :type args_maker: Callable
+        :param cut_rule: 裁剪规则
+        :type cut_rule: Optional[Callable[[str], list[str]]]
+        :param lead_char: 领导符
+        :type lead_char: Optional[types.LeadChar]
+        :param description: 指令的描述
+        :type description: str
+        :param usage: 指令的用法 (如果第一个字符是%, 则自动添加指令名)
+        :type usage: str
+        :param cmd_list: 指令注册表
+        :type cmd_list: Optional[types.CommandList]
         """
 
-        if lead_char is None:
-            lead_char = _default_lead_char
         if cmd_list is None:
             cmd_list = DefaultCommandList
         if usage.startswith("%"):
@@ -101,117 +112,130 @@ def default_args_unpacker(*args, func, **kwargs):
 
 
 class RunCommand:
-    def __init__(self,
-                 args_maker: Callable = default_cut_rule,
-                 cut_rule=default_cut_rule,
-                 cmd_list: Union[types.CommandList, None] = None,
-                 lead_char: Union[types.LeadChar, None] = None,
-                 args_unpacker: Callable = None):
+    def __init__(
+            self,
+            *,
+            lead_char: Optional[types.LeadChar] = _default_lead_char,
+            cut_rule: Callable[[str], list[str]] = default_cut_rule,
+            args_maker: Callable = default_cut_rule,
+            args_unpacker: Optional[Callable] = None,
+            cmd_list: Optional[types.CommandList] = None,
+    ):
 
         """
 
         用于运行指令
 
-        :param args_maker: Callable -> list default: str.split
-        :param cut_rule: Callable -> list default: str.split
-        :param cmd_list: CommandList[List]
+        :param lead_char: 指令前缀
+        :type lead_char: Optional[types.LeadChar]
+        :param cut_rule: 指令切割规则
+        :type cut_rule: Callable[[str], list[str]]
+        :param args_maker: 指令参数生成器
+        :type args_maker: Callable
+        :param args_unpacker: 指令参数解包器
+        :type args_unpacker: Optional[Callable]
+        :param cmd_list: 指令列表
+        :type cmd_list: Optional[types.CommandList]
         """
-
-        if cmd_list is None:
-            cmd_list = DefaultCommandList
-        if lead_char is None:
-            lead_char = _default_lead_char
         if args_unpacker is None:
             args_unpacker = default_args_unpacker
+        if cmd_list is None:
+            cmd_list = DefaultCommandList
 
-        self._cut_rule = cut_rule
-        self._cmd_list = cmd_list
         self._lead_char = lead_char
+        self._cut_rule = cut_rule
         self._args_maker = args_maker
         self._args_unpacker = args_unpacker
+        self._cmd_list = cmd_list
 
-    @staticmethod
-    def _clear_lead_char(string: str, lead_char: types.LeadChar) -> str:
+    def _clear_lead_char(self, string: str, lead_char: Optional[types.LeadChar] = Never) -> str:
         """
-
         清除领导符
 
-        :param string: str: 原指令
-        :param lead_char: LeadChar: 领导符列表
-        :return: str: 根指令
+        :param string: 原指令
+        :type string: str
+        :param lead_char: 领导符列表
+        :type lead_char: Union[types.LeadChar, None]
+        :return: 根指令
+        :rtype: str
         """
-        for char in lead_char:
-            try:
-                temp_str = string[:len(char)]  # 可能会抛出ValueError
-                if temp_str != char:  # 不为正确的领导符就报错（避免else里面执行
-                    raise ValueError
-            except ValueError:
-                pass
-            else:
-                return string[0 + len(char):]  # 返回根指令字符串
-        raise errors.LeadCharNotFindError
+        if lead_char is Never:
+            lead_char = self._lead_char
+        if lead_char is None:
+            return string
 
-    def _get_command_obj(self, string: str, cut_rule) -> Union[dict, None]:
+        for char in lead_char:
+            if string[:len(char)] != char:
+                continue
+
+            return string[len(char):]
+        raise errors.LeadCharNotFindError(
+            f"lead char {lead_char} not find in {string}"
+        )
+
+    def _get_command_obj(
+            self,
+            string: str,
+            cut_rule: Optional[Callable[[str], list[str]]] = None
+    ) -> Union[dict, None]:
         """
 
         获取指令对象
 
-        :param string: str: 指令名
-        :param cut_rule: Callable: 剪裁规则
-        :return: Union[bool, dict]: 指令对应的对象
+        :param string: 指令名
+        :type string: str
+        :param cut_rule: 指令切割规则
+        :type cut_rule: Callable[[str], list[str]]
+        :return: 指令对应的对象
+        :rtype: Union[bool, dict]
         """
+        if cut_rule is None:
+            cut_rule = self._cut_rule
+
         try:
             first_word = cut_rule(string)[0]  # 获取根指令
         except IndexError:
             return None
 
-        cmds = set(self._cmd_list.keys())  # 用集合加速查找
+        cmds = self._cmd_list.keys()
         if first_word in cmds:  # 查找指令
             return self._cmd_list[first_word]  # 返回找到dict的对象
         return None
 
     def run_by_str(self, string: str, op_level: Union[types.OperateLevel, float], *args, **kwargs):
         """
-
         以字符串运行指令
 
-        :param string: str: 原始指令字符串
-        :param op_level: Union[OperateLevel, float]: 权限等级
+        :param string: 原始指令字符串
+        :type string: str
+        :param op_level: 权限等级
+        :type op_level: Union[OperateLevel, float]
         :param args: 额外向指令的位置参数
         :param kwargs: 额外向指令的关键字参数
-        :return object: 指令运行后返回的返回值
+        :return: 指令运行后返回的返回值
+        :rtype: Any
         """
-        try:
-            no_lead_char = self._clear_lead_char(string, self._lead_char)  # 获取无领导符字符串
-            cmd_obj = self._get_command_obj(no_lead_char, self._cut_rule)  # 获取指令对象
+        no_lead_char = self._clear_lead_char(string)
+        cmd_obj = self._get_command_obj(no_lead_char)
+        if cmd_obj is None:
+            raise errors.CommandNotFindError(command_name=string)
 
-            self._clear_lead_char(string, cmd_obj["lead_char"])  # 是否为正确格式的领导符（没找到会抛出 LeadCharNotFindError
-            # 如果cmd_obj为False则会因为没有__getitem__而抛出TypeError
+        self._clear_lead_char(string, cmd_obj["lead_char"])
+        temp_obj = self._get_command_obj(no_lead_char, cmd_obj["cut_rule"])
+        if cmd_obj != temp_obj:
+            raise errors.CommandNotFindError(command_name=string)
 
-            temp_obj = self._get_command_obj(no_lead_char, cmd_obj["cut_rule"])
-            if cmd_obj != temp_obj:
-                raise TypeError
+        if op_level < cmd_obj["op_level"]:
+            raise errors.DontHavePermissionError(level_need=cmd_obj["op_level"], level_now=op_level)
 
-        except TypeError:  # 如果没找到指令 就 进行报错
-            raise errors.CommandNotFindError(
-                command_name=string
-            )
-        except errors.LeadCharNotFindError:
-            raise errors.CommandNotFindError(
-                command_name=string
-            )
+        global_maker_result = self._args_maker(string=string, cmd_obj=cmd_obj, *args, **kwargs)
 
-        if op_level < cmd_obj["op_level"]:  # 执行权限检查
-            raise errors.DontHavePermissionError(level_need=cmd_obj["op_level"], level_now=op_level)  # 如果没权限 就 进行报错
+        local_maker = cmd_obj["args_maker"]
+        local_maker_result = local_maker(string=global_maker_result, cmd_obj=cmd_obj, *args, **kwargs)
 
-        global_maker_ret = self._args_maker(string=string, cmd_obj=cmd_obj, *args, **kwargs)  # 拿到全局
+        ret = self._args_unpacker(local_maker_result, func=cmd_obj["func"])
 
-        maker = cmd_obj["args_maker"]  # 拿到局部
-        maker_ret = maker(string=global_maker_ret, cmd_obj=cmd_obj, *args, **kwargs)
-
-        ret = self._args_unpacker(maker_ret, func=cmd_obj["func"])  # 解包并运行
-
-        return ret  # 返回执行结果
+        return ret
 
     def __call__(self, string: str, op_level: Union[types.OperateLevel, float], *args, **kwargs):
 
